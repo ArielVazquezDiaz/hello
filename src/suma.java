2649754d0d9bba3bde20a9c542dcc0380d9b06b5
Ariel Vazquez <22670036@itiguala.tecnm.mx>
2023-02-08 07:52:19 -0600

    Practica 1, Ariel Vazquez

---
+++ src/suma.java
@@ -0,0 +1,10 @@
+public class suma {
+    public static void main(String[] args) {
+
+        int num2 = 2;
+        int num1 = 2;
+        int suma;
+        suma = num1 + num2;
+        System.out.println("La suma es: " + suma);
+    }
+}
