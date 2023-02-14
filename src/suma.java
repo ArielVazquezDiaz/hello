36aad94999c1d203f7e2017aef176062b741b464
Ariel Vazquez <22670036@itiguala.tecnm.mx>
2023-02-13 19:12:00 -0600

    Se declara la calse guitarra

--- src/suma.java
+++ src/suma.java
@@ -1,8 +1,8 @@
 public class suma {
     public static void main(String[] args) {
 
-        int num2 = 2;
         int num1 = 2;
+        int num2 = 2;
         int suma;
         suma = num1 + num2;
         System.out.println("La suma es: " + suma);
