9c5cf6cce9ec42028e01ff6c5bbbefdd864da0a6
Ariel Vazquez <22670036@itiguala.tecnm.mx>
2023-02-15 07:35:28 -0600

    Se crean las clases

--- src/Consultorio.java
+++ src/Consultorio.java
@@ -4,4 +4,35 @@ public class Consultorio {
     private boolean scale;
     private boolean OfficialDocumentation;
     private boolean MedicalRecords;
+
+    public boolean getMedicalRecords() {
+        return MedicalRecords;
+    }
+    public void setMedicalRecords() {
+        this.MedicalRecords = MedicalRecords;
+    }
+    public boolean getOfficialDocumentation() {
+        return OfficialDocumentation;
+    }
+    public void setOfficialDocumentation() {
+        this.OfficialDocumentation = OfficialDocumentation;
+    }
+    public boolean getScale() {
+        return scale;
+    }
+    public void setScale() {
+        this.scale = scale;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/Joya.java
+++ src/Joya.java
@@ -4,4 +4,35 @@ public class Joya {
     private double carats;
     private String material;
     private double weight;
+
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight() {
+        this.weight = weight;
+    }
+    public String getMaterial() {
+        return material;
+    }
+    public void setMaterial() {
+        this.material = material;
+    }
+    public double getCarats() {
+        return carats;
+    }
+    public void setCarats() {
+        this.carats = carats;
+    }
+    public String getMix() {
+        return mix;
+    }
+    public void setMix() {
+        this.mix = mix;
+    }
+    public String getQuality() {
+        return quality;
+    }
+    public void setQuality() {
+        this.quality = quality;
+    }
 }
