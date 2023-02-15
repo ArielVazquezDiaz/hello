fa55ae76d56d0b5623b06e3b62221bd7f1674467
Ariel Vazquez <22670036@itiguala.tecnm.mx>
2023-02-15 07:29:11 -0600

    Se crean las clases

--- src/Animal.java
+++ src/Animal.java
@@ -4,4 +4,36 @@ public class Animal {
     private String genere;
     private String habitad;
     private String length;
+
+    public String getLength() {
+        return length;
+    }
+    public void setLength(String length) {
+        this.length = length;
+    }
+    public String getHabitad() {
+        return habitad;
+    }
+    public void setHabitad(String habitad) {
+        this.habitad = habitad;
+    }
+    public String getGenere() {
+        return genere;
+    }
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+    public String getSpecie() {
+        return specie;
+    }
+    public void setSpecie(String specie) {
+        this.specie = specie;
+    }
+    public String getGroup() {
+        return group;
+    }
+    public void setGroup() {
+        this.group = group;
+    }
 }
+
--- src/Fruta.java
+++ src/Fruta.java
@@ -4,4 +4,35 @@ public class Fruta {
         private String flavor;
         private double weight;
         private String quantity;
+
+        public String getQuantity() {
+                return quantity;
+        }
+        public void setQuantity() {
+                this.quantity = quantity;
+        }
+        public double getWeight() {
+                return weight;
+        }
+        public void setWeight(double weight) {
+                this.weight = weight;
+        }
+        public String getFlavor() {
+                return flavor;
+        }
+        public void setFlavor(String flavor) {
+                this.flavor = flavor;
+        }
+        public String getColor() {
+                return color;
+        }
+        public void setColor(String color) {
+                this.color = color;
+        }
+        public String getType() {
+                return type;
+        }
+        public void setType() {
+                this.type = type;
+        }
 }
